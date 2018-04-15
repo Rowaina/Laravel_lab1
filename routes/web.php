2bda32cf7c3161efb41e28cf791a9237ca24d479
rowaina <e.rowainaahdy@gmail.com>
2018-04-15 18:47:53 +0200

    add auth

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts','PostsController@index')->name('posts.index');
+Route::get('/posts','PostsController@index')->name('posts.index')->middleware('auth');
 Route::get('posts/create','PostsController@create');
 Route::post('posts','PostsController@store');
 Route::get('posts/{id}', 'PostsController@show');
 Route::get('posts/{id}/edit', 'PostsController@edit');
 Route::put('posts/{id}', 'PostsController@update');
 Route::delete('posts/{id}', 'PostsController@destory');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
